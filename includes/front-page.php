2c269a5f1278ef355f58af3c1f67a174e92f373e
refaktorco <refaktor.co@gmail.com>
2012-12-24 10:06:11 -0800

    -Service icon hover/transition effect

--- includes/front-page.php
+++ includes/front-page.php
@@ -36,19 +36,19 @@
     <div id="services" class="row">
       <div class="twelve columns">
         <div class="row">
-          <div class="three columns">
+          <div class="three columns services_wrap">
             <i class="icon-picture"></i><h3>MINIMAL DESIGN</h3>
             <div class="services_text">Clean and light-weight</div>
           </div>
-          <div class="three columns">
+          <div class="three columns services_wrap">
             <i class="icon-qrcode"></i><h3>MOBILE READY</h3>
              <div class="services_text">Looks great on any device</div>
           </div>
-          <div class="three columns">
+          <div class="three columns services_wrap">
             <i class="icon-heart"></i><h3>EXPERT SUPPORT</h3>
             <div class="services_text">We're here to help you</div>
           </div>
-          <div class="three columns">
+          <div class="three columns services_wrap">
             <i class="icon-cogs"></i><h3>THEME OPTIONS</h3>
             <div class="services_text">Total control of your site</div>
           </div>
