a227bb7ccca6a0597c2738a13c2a0dc13459ebff
refaktorco <refaktor.co@gmail.com>
2013-05-20 12:57:02 -0700

    -Footer tweak.

--- includes/footer.php
+++ includes/footer.php
@@ -35,41 +35,31 @@ function haiku_footer($page){
     </div>  
     <?php endif; ?>
       
-    <?php if (theme_get_setting('enable_primary_footer') == '1') { ?>
+    <?php if( (render($page['footer_1'])) OR (render($page['footer_2'])) OR (render($page['footer_3'])) ) { ?>
     <div class="row">
     
       <div class="four columns">
-        <?php if(!$page['footer_1']) {?>
-        <h2>Footer Block 1</h2>
-        <?php } else { print render($page['footer_1']); }?>   
-      </div> 
-  
-      <div class="four columns">
-        <?php if(!$page['footer_2']) {?>
-        <h2>Footer Block 2</h2>
-        <?php } else { print render($page['footer_2']); }?>  
-      </div>
-      
-                
-      <div class="four columns">
-        <?php if(!$page['footer_3']) {?>
-        <h2>Footer Block 3</h2>        
-        <?php } else { print render($page['footer_3']); }?>      
-      </div>
+	      <?php print render($page['footer_1']); ?>
+	    </div> 
+	
+	    <div class="four columns">
+	      <?php print render($page['footer_2']); ?>
+	    </div>
+	    
+	    <div class="four columns">
+	      <?php print render($page['footer_3']); ?>
+	    </div>
       
     </div> 
     <?php } ?> 
     
-    <?php if (theme_get_setting('enable_secondary_footer') == '1') { ?>
-    <div class="row">
-      <div class="twelve columns"> 
-        <?php if(!$page['footer_full']) {?>    
-        <hr>
-        <h6 class="after_footer"><?php echo theme_get_setting('secondary_footer_text'); ?></h6>
-        <?php } else { print render($page['footer_full']); }?>  
-      </div> 
-    </div>
-    <?php } ?>
+   <?php if ( (render($page['footer_full'])) ) { ?>		
+	 <div class="row">
+	   <div class="twelve columns"> 
+	     <?php print render($page['footer_full']); ?>  
+	   </div> 	
+	 </div>
+	 <?php } ?> 
   
   </div>
   <!-- end footer --> 
